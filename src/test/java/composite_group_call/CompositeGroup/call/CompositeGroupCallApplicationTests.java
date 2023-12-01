adecc53f7c1cacdfae93ab0ad608d37488e6140d
Aarya <tapaswiaarya@gmail.com>
2023-12-01 11:45:49 +0530

    Initial Commit

---
+++ src/test/java/composite_group_call/CompositeGroup/call/CompositeGroupCallApplicationTests.java
@@ -0,0 +1,13 @@
+package composite_group_call.CompositeGroup.call;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeGroupCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
